5b15efedc66f0859c68dcb4aef1409cfd79d4c34
fzlngh <nugrahafazila@gmail.com>
2026-07-20 13:21:09 +0700

    update manifest

--- src/app/Manifest.ts
+++ src/app/Manifest.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "KIR EROBO — Kelompok Ilmiah Remaja SMKN 1 Jakarta",
+    name: "KIR EROBO Kelompok Ilmiah Remaja SMKN 1 Jakarta",
     short_name: "KIR EROBO",
     description:
       "Wadah bagi siswa SMKN 1 Jakarta untuk meneliti, berinovasi, dan menciptakan solusi bagi masa depan melalui divisi AI, Rekayasa Teknologi, dan Science.",
